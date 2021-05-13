e4acf4b519092ac747ca7d437aaffca00019c3d3
Faustino Ducoli <ducoli.faustino@gmail.com>
2021-05-13 12:19:10 +0200

    inizio progetto

---
+++ src/main/java/com/mycompany/_piscina_ducoli/Main.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany._piscina_ducoli;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author FaustinoDucoli
+ */
+public class Main 
+{
+    public static void main(String[] args)
+    {
+        int sceltaUtente =-1;
+        Scanner tastiera= new Scanner(System.in);
+        String[] vociMenu= new String[4];
+        
+        vociMenu[0] = "Esci";
+        vociMenu[1] = "Aggiungi cliente";
+        vociMenu[2] = "Visualizza clienti presenti";
+        vociMenu[3] = "Memorizza l'uscita di un cliente";
+     
+    }
+}
